d7790fe912c94e867bc997c7fbd313506e0fb8e5
Tom Milliman <thomas.milliman@unh.edu>
2017-06-07 13:20:20 -0400

    change development status and remove windows support

--- setup.py
+++ setup.py
@@ -42,12 +42,12 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
+        # 'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         # uncomment if you test on these interpreters:
