713f2bf02dbbc4ec2a00d545e3db6ff615c1cf12
Thomas Milliman <thomas.milliman@unh.edu>
2019-04-03 15:41:17 -0400

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='vegindex',
-    version='0.5.2',
+    version='0.5.3',
     license='MIT',
     description='Python tools for generating vegetation index timeseries from PhenoCam images.',
     long_description='%s\n%s' % (
