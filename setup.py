101e03d6112004e9ac45a2e8a2cd665824be924d
Thomas Milliman <thomas.milliman@unh.edu>
2021-11-24 22:20:09 -0500

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name="vegindex",
-    version="0.8.1",
+    version="0.8.2",
     license="MIT",
     description="Python tools for generating vegetation index timeseries from PhenoCam images.",
     long_description="%s\n%s"
--- src/vegindex/__init__.py
+++ src/vegindex/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Thomas Milliman"""
 __email__ = "thomas.milliman@unh.edu"
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 import os
 
